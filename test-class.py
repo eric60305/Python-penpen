79c84e0716d2dc290dcdb40f4e7cab44b70d4b6a
eric60305 <eric60305@users.noreply.github.com>
2020-10-18 20:41:44 +0800

    20201018 update

--- test-class.py
+++ test-class.py
@@ -1,4 +1,16 @@
 # 定義類別、與類別屬性(封裝在類別中的變數和函式)
-
+#定義一個類別IO. 有兩個屬性 supportedSrcs 和 read
+class IO:
+    supportedSrcs = ["console", "file","e"]
+    def read(src):
+        if src not in IO.supportedSrcs:
+            print("No supported")
+        else:
+            print("Read from",src)
 
 # 使用類別
+print(IO.supportedSrcs)
+IO.read("file")
+IO.read("e")
+IO.read("console")
+IO.read("ce")
